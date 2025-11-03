1264beb5672692dd6658d024b2a878d506e15760
Dipta-b <diptabanik0@gmail.com>
2025-11-03 23:29:52 +0600

    class_constructor

---
+++ Module3/class_contstructor.js
@@ -0,0 +1,17 @@
+class Counter {
+    constructor(count) {
+        this.count = count;
+    }
+    add(amount) {
+        this.count = this.count + amount
+    }
+    print() {
+        console.log(this.count)
+    }
+}
+
+const counter1 = new Counter(0);
+counter1.add(2);
+counter1.add(3);
+counter1.print()
+//this.count += amount; because  updates property count . it increases the existing value by amount  
